0e75a4ffe3aa80feb183320bf847ecaec3dc62e1
mrifainestli7 <101726009+mrifainestli7@users.noreply.github.com>
2022-03-18 14:06:06 +0700

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int input(int &x, int &y) 
-	cout<<"Bilangan Kedua: ";cin>>y
+int input(float &x, float &y) {
+	cout<<"Bilangan Pertama: ";cin>>x;
+	cout<<"Bilangan Kedua: ";cin>>y;
 }
 void menuTambah() {
-	int x,y;
+	float x,y;
 	input(x,y);
-	cout<<"Hasil "<<x<<" + "<<y<<" = "<<x+y;
+	cout<<"Hasil "<<x<<" + "<<y<<" = "<<x+y<<endl;
 }
 
 void menuKurang() {
-	int x, y;
+	float x, y;
 	input(x,y);
-	cout<<"Hasil "<<x<<" - "<<y<<" = "<<x-y;
+	cout<<"Hasil "<<x<<" - "<<y<<" = "<<x-y<<endl;
 }
 
 void menuKali() {
-	int x, y;
+	float x, y;
 	input(x,y);
-	cout<<"Hasil "<<x<<" X "<<y<<" = "<<x*y;
+	cout<<"Hasil "<<x<<" X "<<y<<" = "<<x*y<<endl;
 }
 
 void menuBagi() {
-	int x, y;
+	float x, y;
 	input(x,y);
-	cout<<"Hasil "<<x<<" / "<<y<<" = "<<x/y;
+	cout<<"Hasil "<<x<<" / "<<y<<" = "<<x/y<<endl;
 }
 
 int main(){
 	int pil;
-	cout<<"PROGRAM KALKULATOR"<<endl;
+	do{
+	system("CLS");
+	cout<<" PROGRAM KALKULATOR"<<endl;
+	cout<<"---------------------"<<endl;
 	cout<<"Menu Operasi : "<<endl;
 	cout<<"  1. Penambahan"<<endl;
 	cout<<"  2. Pengurangan"<<endl;
 	cout<<"  3. Perkalian"<<endl;
 	cout<<"  4. Pembagian"<<endl;
+	cout<<"  5. Keluar Program"<<endl;
+	cout<<"---------------------"<<endl;
 	cout<<"Pilih Menu No : ";cin>>pil;
 	if(pil == 1) {
 		menuTambah();
@@ -49,8 +55,7 @@ int main(){
 	else if(pil == 4) {
 		menuBagi();
 	}
-	else {
-		cout<<"Input tidak valid";
-	}
- return 0;
+	system("PAUSE");
+    }
+    while (pil != 5);
 }
